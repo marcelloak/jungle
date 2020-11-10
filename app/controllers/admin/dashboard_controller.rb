0091df410304b13ff9dc49e39cdce488319c578f
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-10 16:31:55 -0700

    Created product and category counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
   
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
